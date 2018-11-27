cd4d2ccc90c35703330fc91c745428c834b6f11b
Pierre MOUCHEL <pierre.mouchel@imie.fr>
2018-11-27 19:54:35 +0100

    'test-post'

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var bodyParser = require("body-parser");
 
 var app = express();
 
 app.use(express.static(__dirname+'/'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
 app.listen(8080);
+
+app.post('/connect.html', function(request, response) {
+  var login = request.body.connectLogin;
+  var password = request.body.connectPassword;
+  console.log(login + " " + password);
+});
