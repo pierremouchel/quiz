04e09bf2aacdcd4549c7b9e0d38b4910460c5455
Pierre MOUCHEL <pierre.mouchel@imie.fr>
2018-11-28 00:46:25 +0100

    'test_erreur_modale'

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 var express = require('express');
 var bodyParser = require("body-parser");
+var validator = require('validator');
 
 var app = express();
 
 app.use(express.static(__dirname+'/'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-    res.render('index.ejs');
+app.get('/', function(request, response) {
+    response.render('index.ejs',{error:0});
 });
 
 app.listen(8080);
 
-app.post('/connect.html', function(request, response) {
+app.post('/signin.html', function(request, response) {
   var login = request.body.connectLogin;
   var password = request.body.connectPassword;
   console.log(login + " " + password);
+});
+
+app.post('/signup.html', function(request, response) {
+  var login = request.body.login;
+  var password = request.body.password;
+  var confpassword = request.body.confpassword;
+  var email = request.body.email;
+  var confemail = request.body.confemail;
+  var country = request.body.country;
+
+  if(validator.isEmpty(login) || validator.isEmpty(password) || validator.isEmpty(confpassword) || validator.isEmpty(email) || validator.isEmpty(confemail) || validator.isEmpty(country)){
+    response.redirect('/?error=' + 1);
+  }
+  console.log(login + " " + password + " " + confpassword + " " + email + " " + confemail + " " + country);
 });
