85c2ef16fb311cf0d3cbb3152921932a54686dc6
Pierre <pierre.mouchel@imie.fr>
2018-11-28 09:59:07 +0100

    'test_message_erreur'

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var bodyParser = require("body-parser");
 var validator = require('validator');
+const Entities = require('html-entities').AllHtmlEntities;
+
+const entities = new Entities();
+
+//console.log(entities.encode('<>"&©®∆'));
+
+function validateSignup(login,password,confpassword,email,confemail,country) {
+  if(validator.isEmpty(login) || validator.isEmpty(password) || validator.isEmpty(confpassword) || validator.isEmpty(email) || validator.isEmpty(confemail) || validator.isEmpty(country)){
+    error = 'Tout les champs ne sont pas complets !';
+  } else if (/\s/.test(login)) {
+    error = 'Votre pseudo ne doit pas contenir d\'espace !';
+  } else if (login.length < 5) {
+    error = 'Votre pseudo doit contenir 5 caractères minimum !';
+  }
+}
 
 var app = express();
 
@@ -8,10 +23,8 @@ app.use(express.static(__dirname+'/'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(request, response) {
-    response.render('index.ejs',{error:0});
+  response.render('index.ejs',{error:''});
 });
-
-app.listen(8080);
 
 app.post('/signin.html', function(request, response) {
   var login = request.body.connectLogin;
@@ -27,8 +40,10 @@ app.post('/signup.html', function(request, response) {
   var confemail = request.body.confemail;
   var country = request.body.country;
 
-  if(validator.isEmpty(login) || validator.isEmpty(password) || validator.isEmpty(confpassword) || validator.isEmpty(email) || validator.isEmpty(confemail) || validator.isEmpty(country)){
-    response.redirect('/?error=' + 1);
-  }
+  validateSignup(login,password,confpassword,email,confemail,country);
+  response.render('index.ejs',{error:error});
+
   console.log(login + " " + password + " " + confpassword + " " + email + " " + confemail + " " + country);
 });
+
+app.listen(8080);
