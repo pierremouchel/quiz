b4aaf7d06c6ad41e03941860951522758cdbf036
Pierre <pierre.mouchel@imie.fr>
2018-12-03 11:46:48 +0100

    test.js

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
-var async = require('async');
-var mysql = require('mysql');
+var mysql      = require('mysql');
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -10,28 +9,22 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err){});
 
-function getLoginRequest() {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      connection.query("SELECT * FROM broquiz_user WHERE user_login = 'michel'", function (err, result, fields) {
-        if (err) {
-          reject(new Error('Request error'));
-        } else {
-          resolve(result);
-        }
-      });
-    }, 0);
+let promise = new Promise( (succes,error) => {
+  connection.query("SELECT * FROM broquiz_user WHERE user_login = 'michel'", function (err, result, fields) {
+    if (result[0] != undefined) {
+      succes(result)
+    } else {
+      error('non')
+    }
   });
-}
+})
 
-async function getLogin() {
-  let login;
-  try {
-    login = await getLoginRequest();
-  } catch (err) {
-    login = 'Request error';
-  }
-  console.log(login);
-}
-
-getLogin();
+promise
+.then(function(succes) {
+  console.log(succes);
+  //throw error;
+})
+.catch(function(error) {
+  console.log('There has been a problem with your fetch operation: ');
+  //throw error;
+});
