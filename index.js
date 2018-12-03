03982f0998bb832c5fc79e667497cfea6ce0b892
Pierre <pierre.mouchel@imie.fr>
2018-12-03 16:30:46 +0100

    'connexion_fonctionnelle'

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var validator = require('validator');
 const Entities = require('html-entities').AllHtmlEntities;
-var mysql      = require('mysql');
+var mysql = require('mysql');
 const crypto = require('crypto');
 var datetime = require('node-datetime');
 
@@ -54,23 +54,78 @@ function getDate() {
 }
 
 var app = express();
-var form_error;
+var signup_error;
+var signin_error;
+var error_type = 0;
 
 app.use(express.static(__dirname+'/'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(request, response) {
-  if (typeof form_error == 'undefined') {
-    response.render('index.ejs');
-  } else {
-    response.render('index.ejs',{form_error:form_error,login:login,password:password,confpassword:confpassword,email:email,confemail:confemail,country:country});
+  if (error_type == 0) {
+    response.render('index.ejs',{error_type:error_type});
+  } else if (error_type == 1) {
+    response.render('index.ejs',{signin_error:signin_error,error_type:error_type,signin_login:signin_login,signin_password:signin_password});
+  } else if (error_type == 2) {
+    response.render('index.ejs',{signup_error:signup_error,error_type:error_type,login:login,password:password,confpassword:confpassword,email:email,confemail:confemail,country:country});
   }
 
 });
 
 app.post('/signin.html', function(request, response) {
-  login = request.body.connectLogin;
-  password = request.body.connectPassword;
+  signin_login = request.body.connectLogin.trim();
+  signin_password = request.body.connectPassword.trim();
+
+  var sha512 = function(password, salt){
+    var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+    hash.update(password);
+    var value = hash.digest('hex');
+    return {
+        passwordHash:value
+    };
+  };
+
+  var validationSignin = new Promise((success,error) => {
+    if(validator.isEmpty(signin_login) || validator.isEmpty(signin_password)){
+      error('Tout les champs ne sont pas complets !');
+    } else if (validator.isEmpty(signin_login) == false && validator.isEmail(signin_login)) {
+      connection.query("SELECT user_email, user_password, user_salt FROM broquiz_user WHERE user_email = '"+signin_login+"'", function (err, result, fields) {
+        if (result[0] == undefined) {
+          error('Cet email n\'existe pas !');
+        } else {
+          if (sha512(signin_password, result[0].user_salt).passwordHash == result[0].user_password) {
+            success('Connected');
+          } else {
+            error('Mot de passe éronné !')
+          }
+        }
+      });
+    } else if (validator.isEmpty(signin_login) == false) {
+      connection.query("SELECT user_login, user_password, user_salt FROM broquiz_user WHERE user_login = '"+signin_login+"'", function (err, result, fields) {
+        if (result[0] == undefined) {
+          error('Ce login n\'existe pas !');
+        } else {
+          if (sha512(signin_password, result[0].user_salt).passwordHash == result[0].user_password) {
+            success('Connected');
+          } else {
+            error('Mot de passe éronné !')
+          }
+        }
+      });
+    }
+  });
+
+  validationSignin
+  .then(function(success) {
+    error_type = 0;
+    response.redirect('/');
+    console.log(success);
+  })
+  .catch(function(error) {
+    signin_error = error;
+    error_type = 1;
+    response.redirect('/');
+  });
 });
 
 app.post('/signup.html', function(request, response) {
@@ -81,7 +136,7 @@ app.post('/signup.html', function(request, response) {
   confemail = request.body.confemail.trim();
   country = request.body.country.trim();
 
-  var validation = new Promise((success,error) => {
+  var validationSignup = new Promise((success,error) => {
     if(validator.isEmpty(login) || validator.isEmpty(password) || validator.isEmpty(confpassword) || validator.isEmpty(email) || validator.isEmpty(confemail) || validator.isEmpty(country)){
       error('Tout les champs ne sont pas complets !');
     } else if (/\s/.test(login)) {
@@ -114,7 +169,7 @@ app.post('/signup.html', function(request, response) {
     }
   })
 
-  validation
+  validationSignup
   .then(function(success) {
     login = entities.encode(login);
     hashPwd(password);
@@ -140,12 +195,13 @@ app.post('/signup.html', function(request, response) {
     confemail = undefined;
     country = undefined;
 
-    form_error = undefined;
+    signup_error = undefined;
 
     response.redirect('/');
   })
   .catch(function(error) {
-    form_error = error;
+    signup_error = error;
+    error_type = 2;
     response.redirect('/');
   });
 
