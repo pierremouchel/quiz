40de3d712c1e5be100b0ec91403be7d3b1e65e45
Pierre <pierre.mouchel@imie.fr>
2018-12-05 12:08:16 +0100

    'demande_ami'

--- scripts/add_friend.js
+++ scripts/add_friend.js
@@ -1,14 +1,22 @@
 friend_login = request.body.friendLogin.trim();
 
 var validationFriend = new Promise((success,error) => {
+  console.log(request.session.user_id);
   if(validator.isEmpty(friend_login)){
     error('Le champ est vide !');
-  } else if (validator.isEmpty(friend_login) == false && validator.isEmail(friend_login)) {
+  } else {
     connection.query("SELECT user_id, user_login FROM broquiz_user WHERE user_login = '"+friend_login+"'", function (err, result, fields) {
       if (result[0] == undefined) {
         error('Ce pseudo n\'existe pas !');
       } else {
-        success('Succès');
+        console.log(result[0].user_id);
+        var sql = "INSERT INTO friend (friend1_id, friend2_id, friend_status) VALUES ?";
+        var values = [[request.session.user_id, result[0].user_id, 1]];
+
+        connection.query(sql , [values], function (err, result) {
+          if (err) throw err;
+          success('Demande d\'ami envoyée !');
+        });
       }
     });
   }
@@ -16,9 +24,10 @@ var validationFriend = new Promise((success,error) => {
 
 validationFriend
 .then(function(success) {
+  console.log(success);
   response.redirect('/accueil');
 })
 .catch(function(error) {
   console.log(error);
-  response.redirect('/');
+  response.redirect('/accueil');
 });
